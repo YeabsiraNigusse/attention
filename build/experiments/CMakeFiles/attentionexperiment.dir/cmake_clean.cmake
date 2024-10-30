696e89d5304e3a64f977bb21285755763e2674c6
YeabsiraNigusse <nigusseyeabsira@gmail.com>
2024-10-30 08:58:21 +0300

    added some issues

---
+++ build/experiments/CMakeFiles/attentionexperiment.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/attentionexperiment.dir/ExperimentSetupModule.cc.o"
+  "CMakeFiles/attentionexperiment.dir/ExperimentSetupModule.cc.o.d"
+  "CMakeFiles/attentionexperiment.dir/SentenceGenStimulateAgent.cc.o"
+  "CMakeFiles/attentionexperiment.dir/SentenceGenStimulateAgent.cc.o.d"
+  "libattentionexperiment.pdb"
+  "libattentionexperiment.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/attentionexperiment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
